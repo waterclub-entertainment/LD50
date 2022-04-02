81fffdb47a3916f057671e0a79284a83c0379e51
MDeiml <matthias@deiml.net>
2022-04-02 15:14:52 +0200

    Add walking animation

--- Assets/Components/PlayerControls.cs
+++ Assets/Components/PlayerControls.cs
@@ -5,6 +5,7 @@ public class PlayerControls : MonoBehaviour {
     public float speed = 10.0f;
     public Animator animator;
     public CharacterController characterController;
+    public SpriteRenderer spriteRenderer;
     public float movementMultiplier = 0.0f;
     public bool canControl = true;
     private Vector3 direction; 
@@ -26,6 +27,11 @@ public class PlayerControls : MonoBehaviour {
         }
         if (Input.GetMouseButtonDown(1)) {
             animator.SetTrigger("Dash");
+        }
+        if (direction.x < 0) {
+            spriteRenderer.flipX = true;
+        } else if (direction.x > 0) {
+            spriteRenderer.flipX = false;
         }
         Vector3 velocity = Quaternion.Euler(0, 45, 0) * direction * movementMultiplier * speed;
         characterController.Move(velocity * Time.deltaTime);
