faca3bb589720bd413acbf77ed01be0e2bafbf27
MDeiml <matthias@deiml.net>
2022-04-04 12:32:51 +0200

    Jingle

---
+++ Assets/Components/JingleController.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class JingleController : MonoBehaviour {
+
+    public AudioClip[] jingles;
+
+    void Start() {
+        GetComponent<AudioSource>().PlayOneShot(jingles[Random.Range(0, jingles.Length)]);
+        StartCoroutine(SceneTransition());
+    }
+
+    IEnumerator SceneTransition() {
+        yield return new WaitForSeconds(2);
+        SceneManager.LoadScene("Scenes/Main");
+    }
+
+}
