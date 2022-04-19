fd5d8873112ba6df43a39997a6ab953d7e7e8426
LordBlackheart <82619092+LordBlackheart@users.noreply.github.com>
2022-04-19 22:12:41 +0200

    added stuff in main

---
+++ Assets/TestAddIns/SineVFX/GalaxyMaterials/Resources/Scripts/SineCameraControllerGalaxyMaterials.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SineCameraControllerGalaxyMaterials : MonoBehaviour
+{
+
+    public new Camera camera;
+    public Transform basePivot;
+    public Transform farPivot;
+    public float scrollSpeed = 10f;
+    public float rotationSpeed = 10f;
+    public float rotationAmount = 2f;
+    [Range(10f, 40f)]
+    public float maximumAngle = 20f;
+
+    private float closeFar = 0.5f;
+    private float closeFarLerp = 0.5f;
+    private Vector3 mouseAxisToVector;
+    private float x;
+    private float y;
+    private Quaternion rotation;
+    private bool rotationPossible = false;
+
+    void Start()
+    {
+        rotation = gameObject.transform.localRotation;
+        mouseAxisToVector = new Vector3(0f, 0f, 0f);
+    }
+
+    void Update()
+    {
+        if (Input.GetAxis("Mouse ScrollWheel") > 0f)
+        {
+            if (closeFar < 1f)
+            {
+                closeFar += 0.1f;
+            }
+            if (closeFar > 1f)
+            {
+                closeFar = 1f;
+            }
+        }
+        else if (Input.GetAxis("Mouse ScrollWheel") < 0f)
+        {
+            if (closeFar > 0f)
+            {
+                closeFar -= 0.1f;
+            }
+            if (closeFar < 0f)
+            {
+                closeFar = 0f;
+            }
+        }
+        closeFarLerp = Mathf.Lerp(closeFarLerp, closeFar, Time.deltaTime * scrollSpeed);
+        camera.transform.position = Vector3.Lerp(farPivot.position, basePivot.position, closeFarLerp);
+
+        if (Input.GetMouseButton(0))
+        {
+            rotationPossible = true;
+        }
+        else
+        {
+            rotationPossible = false;
+        }
+
+        if (rotationPossible == true)
+        {
+            rotation = gameObject.transform.localRotation;
+            x = rotation.eulerAngles.x + Input.GetAxis("Mouse Y") * rotationAmount;
+            if (x > maximumAngle && x < 180)
+            {
+                x = maximumAngle;
+            }
+            if (x < 340f && x > 180f)
+            {
+                x = 340f;
+            }
+            y = rotation.eulerAngles.y + Input.GetAxis("Mouse X") * rotationAmount;
+            mouseAxisToVector.Set(x, y, 0f);
+            rotation.eulerAngles = mouseAxisToVector;
+            gameObject.transform.localRotation = rotation;
+        }
+    }
+}
---
+++ Assets/TestAddIns/SineVFX/GalaxyMaterials/Resources/Scripts/SineUIControllerGalaxyMaterials.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SineUIControllerGalaxyMaterials : MonoBehaviour
+{
+
+    public Transform prefabHolder;
+    public CanvasGroup canvasGroup;
+
+    private Transform[] prefabs;
+    private List<Transform> lt;
+    private int activeNumber = 0;
+
+    private void Start()
+    {
+        lt = new List<Transform>();
+        prefabs = prefabHolder.GetComponentsInChildren<Transform>(true);
+
+        foreach (Transform tran in prefabs)
+        {
+            if (tran.parent == prefabHolder)
+            {
+                lt.Add(tran);
+            }
+        }
+
+        prefabs = lt.ToArray();
+        EnableActive();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            canvasGroup.alpha = 1f - canvasGroup.alpha;
+        }
+        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            ChangeEffect(true);
+        }
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            ChangeEffect(false);
+        }
+    }
+
+    // Turn On active VFX Prefab
+    public void EnableActive()
+    {
+        for (int i = 0; i < prefabs.Length; i++)
+        {
+            if (i == activeNumber)
+            {
+                prefabs[i].gameObject.SetActive(true);
+            }
+            else
+            {
+                prefabs[i].gameObject.SetActive(false);
+            }
+        }
+    }
+
+    // Change active VFX
+    public void ChangeEffect(bool bo)
+    {
+        if (bo == true)
+        {
+            activeNumber++;
+            if (activeNumber == prefabs.Length)
+            {
+                activeNumber = 0;
+            }
+        }
+        else
+        {
+            activeNumber--;
+            if (activeNumber == -1)
+            {
+                activeNumber = prefabs.Length - 1;
+            }
+        }
+
+        EnableActive();
+    }
+}
